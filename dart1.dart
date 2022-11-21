14ffff4bddbb1263ae8a1a07664d24a006ace35b
JaswanthJHK <jaswanthkriz@gmail.com>
2022-11-21 20:56:16 +0530

    initial_commit

---
+++ dart1.dart
@@ -0,0 +1,15 @@
+import 'dart:developer';
+
+void main(List<String> args) {
+  String name = "Anu";
+  int age = 20;
+  int phone = 876543210;
+  double mark = 8.3;
+  String email = "anu@gmail.com";
+  log("----My resume----");
+  log("my name is $name");
+  log("i am $age yrs old");
+  log("my phone number is $phone");
+  log("my $mark cgpa in my graduation");
+  log('my email id is $email');
+}
